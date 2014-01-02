ea875cdfbf9b08d1cc118f06e2f025c3d4543eac
AlexJPZ17 <alexjpz17@gmail.com>
2014-01-02 13:35:59 -0600

    VB Calcular el cos sin y tan

---
+++ Calcular el cos sin y tan/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox_Angulo = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button_Run = New System.Windows.Forms.Button()
+        Me.ComboBox_Funciones = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'TextBox_Angulo
+        '
+        Me.TextBox_Angulo.Location = New System.Drawing.Point(26, 60)
+        Me.TextBox_Angulo.Name = "TextBox_Angulo"
+        Me.TextBox_Angulo.Size = New System.Drawing.Size(75, 20)
+        Me.TextBox_Angulo.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(89, 15)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Ingrese Angulo"
+        '
+        'Button_Run
+        '
+        Me.Button_Run.Location = New System.Drawing.Point(82, 142)
+        Me.Button_Run.Name = "Button_Run"
+        Me.Button_Run.Size = New System.Drawing.Size(75, 23)
+        Me.Button_Run.TabIndex = 3
+        Me.Button_Run.Text = "Run"
+        Me.Button_Run.UseVisualStyleBackColor = True
+        '
+        'ComboBox_Funciones
+        '
+        Me.ComboBox_Funciones.FormattingEnabled = True
+        Me.ComboBox_Funciones.Items.AddRange(New Object() {"Cos", "Sin", "Tan"})
+        Me.ComboBox_Funciones.Location = New System.Drawing.Point(138, 60)
+        Me.ComboBox_Funciones.Name = "ComboBox_Funciones"
+        Me.ComboBox_Funciones.Size = New System.Drawing.Size(89, 21)
+        Me.ComboBox_Funciones.TabIndex = 4
+        Me.ComboBox_Funciones.Text = "Funciones"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 258)
+        Me.Controls.Add(Me.ComboBox_Funciones)
+        Me.Controls.Add(Me.Button_Run)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox_Angulo)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox_Angulo As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button_Run As System.Windows.Forms.Button
+    Friend WithEvents ComboBox_Funciones As System.Windows.Forms.ComboBox
+
+End Class
---
+++ Calcular el cos sin y tan/Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+    Private Sub Trigonometria()
+        'Declaramos las variables
+        Dim angulo, res As Double
+        Dim funcion As String
+
+        'Condición para validar el TextBox_Angulo si este se encuentra vacío 
+        If TextBox_Angulo.Text = "" Then
+            MessageBox.Show("Ingrese un angulo", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            'Coloca el Focus en el TextBox_Angulo
+            TextBox_Angulo.Focus()
+        Else
+            'Función que convierte en un tipo double el valor que se ingrese por 
+            'el TextBox_Angulo y lo almacena en la variable ángulo 
+            angulo = Double.Parse(TextBox_Angulo.Text)
+            'Método para extraer los Item seleccionado en el ComboBox
+            funcion = ComboBox_Funciones.Text
+            'Condición que evalúa la variable función si es igual a las funciones 
+            'seleccionada en el ComboBox 
+            If funcion = "Funciones" Then
+                MessageBox.Show("Seleccione una de las funciones", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                'Select Case nos permitirá controlar
+                'las opciones que escojamos por el ComboBox
+                Select Case funcion
+                    Case "Cos"
+                        'Método que saca el Coseno del dato que este almacenado 
+                        'en la varia ángulo y lo almacena en la variable res
+                        res = Math.Cos(Math.PI * Val(angulo) / 180)
+                        MessageBox.Show("El coseno del angulo " & angulo & " es " & res)
+
+                    Case "Sin"
+                        'Método que saca el Seno del dato que este almacenado 
+                        'en la varia ángulo y lo almacena en la variable res
+                        res = Math.Sin(Math.PI * Val(angulo) / 180)
+                        MessageBox.Show("El Seno del angulo " & angulo & " es " & res)
+
+                    Case "Tan"
+                        'Método que saca el Tangente del dato que este almacenado 
+                        'en la varia ángulo y lo almacena en la variable res
+                        res = Math.Tan(Math.PI * Val(angulo) / 180)
+                        MessageBox.Show("El Tangente del angulo " & angulo & " es " & res)
+                End Select
+            End If
+
+        End If
+
+    End Sub
+   
+    Private Sub Button_Run_Click(sender As Object, e As EventArgs) Handles Button_Run.Click
+        Trigonometria()
+    End Sub
+End Class
